081a2b3f185359c72e6c151aa5d08a772ecc9b49
kestulka <kgirdiunas0@gmail.com>
2024-02-12 16:07:09 +0200

    boiler + models

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
+const connectToDB = require("./config/db");
+
+connectToDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.options("*", cors());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+
+app.listen(process.env.PORT, () =>
+  console.log(`server is running on port ${process.env.PORT}`),
+);
