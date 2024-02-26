e9d278270180f1ba4ad1405296f2ab922d63f88d
Laurynas <sapnagis.laurynasls@gmail.com>
2024-02-26 16:56:35 +0200

    Bugfix

--- frontend/js/about.js
+++ frontend/js/about.js
@@ -1,17 +1,9 @@
 const dashbtn = document.getElementById('dashboard')
-const contactbtn = document.querySelector('#contact-us')
 const dashboard = document.getElementById('dashboard-home')
 const UserID = localStorage.getItem('userID')
 dashbtn.addEventListener('click', () => {
     if (UserID) {
         window.location.href = 'dashboard.html'
-    } else {
-        window.location.href = 'index.html#top'
-    }
-})
-contactbtn.addEventListener('click', () => {
-    if (UserID) {
-        window.location.href = 'contact.html'
     } else {
         window.location.href = 'index.html#top'
     }
