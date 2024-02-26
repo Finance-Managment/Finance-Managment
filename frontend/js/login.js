de4fa23aa2b2482f0843578a155f0eda57b6461c
kestulka <kgirdiunas0@gmail.com>
2024-02-26 21:23:29 +0200

    bugfixes

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,10 +1,10 @@
-const loginForm = document.getElementById('loginForm');
+const loginForm = document.getElementById('loginForm')
 
 loginForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const email = document.getElementById('loginFormEmailInput').value;
-    const password = document.getElementById('loginFormPasswordInput').value;
+    const email = document.getElementById('loginFormEmailInput').value
+    const password = document.getElementById('loginFormPasswordInput').value
 
     try {
         const response = await fetch('http://localhost:3000/api/users/login', {
@@ -13,20 +13,20 @@ loginForm.addEventListener('submit', async (event) => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ email, password }),
-        });
+        })
 
-        const data = await response.json();
+        const data = await response.json()
 
         if (response.ok) {
-            localStorage.setItem('userRole', data.role);
-            localStorage.setItem('userID', data._id);
-            localStorage.setItem('userToken', data.token);
+            localStorage.setItem('userRole', data.role)
+            localStorage.setItem('userID', data._id)
+            localStorage.setItem('userToken', data.token)
 
-            window.location.href = "dashboard.html";
+            window.location.href = '/dashboard'
         } else {
-            alert("Negerai ivesti duomenys");
+            alert('Negerai ivesti duomenys')
         }
     } catch (error) {
-        alert('Error logging in:', error.message);
+        alert('Error logging in:', error.message)
     }
-});
+})
--- frontend/js/logoutBtn.js
+++ frontend/js/logoutBtn.js
@@ -1,12 +1,11 @@
-const logoutButton = document.getElementById('logoutButton');
+const logoutButton = document.getElementById('logoutButton')
 
 logoutButton.addEventListener('click', (event) => {
+    event.preventDefault()
 
-    event.preventDefault();
+    localStorage.removeItem('userRole')
+    localStorage.removeItem('userID')
+    localStorage.removeItem('userToken')
 
-    localStorage.removeItem('userRole');
-    localStorage.removeItem('userID');
-    localStorage.removeItem('userToken');
-
-    window.location.href = 'index.html';
-});
+    window.location.href = '/'
+})
