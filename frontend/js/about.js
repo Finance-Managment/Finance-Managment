620982178368692c25d214fe269e1a40a51d3a0c
Laurynas <sapnagis.laurynasls@gmail.com>
2024-02-26 12:14:06 +0200

    Bugfix/home/finance

--- frontend/js/about.js
+++ frontend/js/about.js
@@ -1,5 +1,6 @@
 const dashbtn = document.getElementById('dashboard')
 const contactbtn = document.querySelector('#contact-us')
+const dashboard = document.getElementById('dashboard-home')
 const UserID = localStorage.getItem('userID')
 dashbtn.addEventListener('click', () => {
     if (UserID) {
@@ -11,6 +12,13 @@ dashbtn.addEventListener('click', () => {
 contactbtn.addEventListener('click', () => {
     if (UserID) {
         window.location.href = 'contact.html'
+    } else {
+        window.location.href = 'index.html#top'
+    }
+})
+dashboard.addEventListener('click', () => {
+    if (UserID) {
+        window.location.href = 'dashboard.html'
     } else {
         window.location.href = 'index.html#top'
     }
