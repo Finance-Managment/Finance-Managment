d3f136fa3e9a238e4cf3bda696473700f435912a
kestulka <kgirdiunas0@gmail.com>
2024-02-26 21:08:30 +0200

    deploymentas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config()
 const express = require('express')
+const http = require('http')
 const cors = require('cors')
 const errorHandler = require('./backend/middleware/errorHandler.js')
 const connectToDB = require('./backend/config/db.js')
@@ -33,6 +34,26 @@ app.use('/api/savings', require('./backend/routes/savingRoutes.js'))
 
 app.use(errorHandler)
 
-app.listen(process.env.PORT, () =>
-    console.log(`server is running on port ${process.env.PORT}`)
-)
+const port = process.env.PORT || 8080
+app.set('port', port)
+
+app.use(express.static(__dirname + '/frontend'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/frontend/index.html')
+})
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/frontend/html/about.html')
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/frontend/html/contact.html')
+})
+app.get('/dashboard', (req, res) => {
+    res.sendFile(__dirname + '/frontend/html/dashboard.html')
+})
+
+const server = http.createServer(app)
+
+server.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
