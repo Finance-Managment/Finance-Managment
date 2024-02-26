2654091e3c707db3d323397945c294e4ab890039
kestulka <kgirdiunas0@gmail.com>
2024-02-26 18:48:38 +0200

    moved npm to root

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const errorHandler = require('./middleware/errorHandler.js')
-const connectToDB = require('./config/db.js')
+const errorHandler = require('./backend/middleware/errorHandler.js')
+const connectToDB = require('./backend/config/db.js')
 
 connectToDB()
 
@@ -17,23 +17,22 @@ app.use(express.urlencoded({ extended: false }))
 // routes
 
 // users
-app.use('/api/users', require('./routes/userRoutes.js'))
+app.use('/api/users', require('./backend/routes/userRoutes.js'))
 
 // incomes
 
-app.use('/api/incomes', require('./routes/incomeRoutes.js'))
+app.use('/api/incomes', require('./backend/routes/incomeRoutes.js'))
 
 // outcomes
 
-app.use('/api/outcomes', require('./routes/outcomeRoutes.js'))
+app.use('/api/outcomes', require('./backend/routes/outcomeRoutes.js'))
 
 // savings
 
-app.use('/api/savings', require('./routes/savingRoutes.js'))
+app.use('/api/savings', require('./backend/routes/savingRoutes.js'))
 
 app.use(errorHandler)
 
 app.listen(process.env.PORT, () =>
     console.log(`server is running on port ${process.env.PORT}`)
 )
-
